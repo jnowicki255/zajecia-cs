5c355d3daec15e16f79c15fd81d5ec95aaafe31a
Jacek Nowicki <mopikx@gmail.com>
2022-01-22 10:43:48 +0100

    Added second and third project.

--- P02-Objects/Program.cs
+++ P02-Objects/Program.cs
@@ -6,10 +6,15 @@ namespace P02_Objects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Objects!");
+            Book firstBook = new Book("Informatyka w szkole średniej.", "Jan Nowak", 400);
+            Book secondBook = new Book("Krzyżacy", "Sienkiewicz", 1000);
+            Book thirdBook = new Book(secondBook);
+            Book fourthBook = new Book();
 
-            Book firstBook = new Book("Informatyka w szkole średniej.",
-                "Jan Nowak", 400);
+            firstBook.Print();
+            secondBook.Print("secondBook"); // Wywołanie przeciążonej metody
+            thirdBook.Print();
+            fourthBook.Print();
         }
     }
 
@@ -19,11 +24,48 @@ namespace P02_Objects
         public string Author;
         public int PagesCount;
 
+        public Book()
+        {
+            Title = string.Empty;
+            Author = string.Empty;
+            PagesCount = 0;
+        }
+
+        /// <summary>
+        /// Konstruktor standardowy
+        /// </summary>
+        /// <param name="title">Tytuł książki</param>
+        /// <param name="author">Autor książki</param>
+        /// <param name="pagesCount">Liczba stron</param>
         public Book(string title, string author, int pagesCount)
         {
-            Title = title;
-            Author = author;
-            PagesCount = pagesCount;
+            this.Title = title;
+            this.Author = author;
+            this.PagesCount = pagesCount;
+        }
+
+        /// <summary>
+        /// Konstruktor kopiujący
+        /// </summary>
+        /// <param name="book"></param>
+        public Book(Book book)
+        {
+            Title = book.Title;
+            Author = book.Author;
+            PagesCount = book.PagesCount;
+        }
+
+        public void Print()
+        {
+            if (Title == string.Empty)
+                return;
+
+            Console.WriteLine($"Tytuł książki: {Title}, Autor: {Author}, Ilość stron: {PagesCount}");
+        }
+
+        public void Print(string bookName) // Przeciążenie metody Print()
+        {
+            Console.WriteLine($"Książka: {bookName}, Tytuł książki: {Title}, Autor: {Author}, Ilość stron: {PagesCount}");
         }
     }
 }
---
+++ P03-Methods/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace P03_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Przekazanie argumentu przez wartość.
+            string name1 = "JACEK";
+            StringOperation(name1);
+            Console.WriteLine(name1);
+
+            // 2. Przekazanie argumentu przez referencję
+            string name2 = "JACEK";
+            StringOperationRef(ref name2);
+            Console.WriteLine(name2); // W przypadku refencji zmieniona zostaje wartość źródłowa.
+
+            // 3. Przekzanie argumentu przez wyjście
+            string name3;
+            StringOperationOut(out name3);
+            Console.WriteLine(name3);
+
+            // 4. Przekazywanie listy argumentów
+            StringOperationParams("Paweł");
+            StringOperationParams("Jacek", "Adam", "Piotr");
+            StringOperationParams("Marek", "Maciej", "Ryszard", "Zdzisław", "Mieczysław");
+        }
+
+        static void StringOperation(string str)
+        {
+            str = str.ToLower();
+            Console.WriteLine(str);
+        }
+
+        static void StringOperationRef(ref string str)
+        {
+            str = str.ToLower();
+            Console.WriteLine(str);
+        }
+
+        static void StringOperationOut(out string str)
+        {
+            str = "Krzysztof";
+        }
+
+        static void StringOperationParams(params string[] strArray)
+        {
+            StringBuilder strBuilder = new StringBuilder();
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                strBuilder.Append(" ");
+                strBuilder.Append(strArray[i]);
+                strBuilder.Append(" ");
+            }
+            
+            Console.WriteLine(strBuilder.ToString());
+        }
+    }
+}
