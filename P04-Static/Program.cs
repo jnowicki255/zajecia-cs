67470bed51a493e963e04e510ffa2f2d4f87f4dc
Jacek Nowicki <mopikx@gmail.com>
2022-01-22 11:52:09 +0100

    Added fourth and fifth project

---
+++ P04-Static/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace P04_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Utworzenie obiektów typu Person
+            Person person1 = new Person()
+            {
+                FirstName = "Jacek",
+                LastName = "Nowicki",
+                Age = 27,
+                Telephone = "666-666-666"
+            };
+
+            Person person2 = new Person()
+            {
+                FirstName = "John",
+                LastName = "Wick",
+                Age = 42,
+                Telephone = "555-555-555"
+            };
+
+            // 2. Wyświetlenie danych o obiektach
+            person1.PrintData();
+            person2.PrintData();
+
+            // 3. Wykorzystanie zmiennej statycznej\
+            Console.WriteLine($"Imię osoby: {person1.FirstName}"); // Aby użyć pola NIE-statycznego musimy mieć utworzony obiekt.
+            Console.WriteLine($"Każda osoba posiada {Person.NumberOfLegs}"); // Do wykorzyania pola statycznego nie jest potrzebne stworzenie obiektu.
+
+            person1.PrintData();
+            Person.Move(100);
+
+            // 4. Realne zastosowania
+
+            //Math mathObj = new Math();
+            //double cos = mathObj.Cos(x);
+
+            double x = 2;
+            double y = Math.Pow(x, 2);
+            double sin = Math.Sin(x);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string Telephone;
+
+        public static int NumberOfLegs = 2;
+
+        public void PrintData()
+        {
+            Console.WriteLine($"FirstName: {FirstName}; LastName: {LastName}, Age: {Age}," +
+                $" Telephone: {Telephone}; NumberOfLegs: {NumberOfLegs}");
+        }
+
+        public static void Move(int distance)
+        {
+            Console.WriteLine($"Person with {NumberOfLegs} legs has moved {distance} meters");
+        }
+    }
+}
---
+++ P05-Properties/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace P05_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Aston Martin", "DB9", 2007);
+            Console.WriteLine(car1.Brand);
+            Console.WriteLine(car1.Model);
+            //Console.WriteLine(car1.ProductionYear); - Ta linia nie zadziała, ponieważ właściwość ProductionYear posiada tylko setter.
+
+            car1.Brand = "Dacia";
+            Console.WriteLine(car1.Brand);
+            Console.WriteLine(car1.Model);
+            //Console.WriteLine(car1.ProductionYear);
+        }
+    }
+
+    public class Car
+    {
+        private string brand;
+        private string model;
+        private int productionYear;
+
+        public string Brand
+        {
+            get { return brand; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                    return;
+
+                brand = value; 
+            }
+        }
+
+        public string Model
+        {
+            get { return model; }
+        }
+
+        public int ProductionYear
+        {
+            set { productionYear = value; }
+        }
+
+        public int EngineCapacity { get; private set; }
+
+        public Car(string brand, string model, int productionYear)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.productionYear = productionYear;
+            this.EngineCapacity = 6000;
+        }
+    }
+}
