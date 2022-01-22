4179fe505e8a6496ca8848c229ab8a71302d978d
Jacek Nowicki <mopikx@gmail.com>
2022-01-22 12:28:57 +0100

    Added GuessGame

---
+++ P06-GuessNumberGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace P06_GuessNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+        }
+
+        private static void StartGame()
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int number = random.Next(0, 100);
+
+                ReadAndCompareNumber(number);
+
+                Console.Write("Czy chcesz spróbować jeszcze raz? [T/N]");
+                string response = Console.ReadLine();
+
+                if (response != "t")
+                    break;
+            }
+        }
+
+        static void ReadAndCompareNumber(int number)
+        {
+            Console.Write("Podaj swój typ: ");
+            int value;
+
+            do
+            {
+                value = Convert.ToInt32(Console.ReadLine());
+
+                if (value < number)
+                    Console.Write("Twoja liczba jest za mała, jeszcze raz: ");
+                else if (value > number)
+                    Console.Write("Twoja liczba jest za duża, jeszcze raz: ");
+            } 
+            while (value != number);
+
+            Console.WriteLine($"Trafiłeś, wylosowana liczba to: {number}");
+        }
+    }
+}
