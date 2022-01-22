4645efb3895e08273fead32fa3fc4862686a0463
Jacek Nowicki <mopikx@gmail.com>
2022-01-22 08:01:27 +0100

    Added second project

---
+++ P02-Objects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P02_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book firstBook = new Book("Informatyka w szkole średniej.",
+                "Jan Nowak", 400);
+        }
+    }
+
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public int PagesCount;
+
+        public Book(string title, string author, int pagesCount)
+        {
+            Title = title;
+            Author = author;
+            PagesCount = pagesCount;
+        }
+    }
+}
