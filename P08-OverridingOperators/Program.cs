a0dcb00778b88a0434ca1b62a462556d69b2cfae
asc3rr <borysgnacinski@gmail.com>
2022-01-29 12:51:18 +0100

    added ComplexNumber multiplication

--- P08-OverridingOperators/Program.cs
+++ P08-OverridingOperators/Program.cs
@@ -6,14 +6,16 @@ namespace P08_OverridingOperators
     {
         static void Main(string[] args)
         {
-            ComplexNumber c1 = new ComplexNumber(5, 10);
-            ComplexNumber c2 = new ComplexNumber(20, 20);
+            ComplexNumber c1 = new ComplexNumber(4, -1);
+            ComplexNumber c2 = new ComplexNumber(-2, 2);
 
             ComplexNumber suma = c1 + c2;
             ComplexNumber roznica = c1 - c2;
+            ComplexNumber iloczyn = c1 * c2;
 
             Console.WriteLine($"Suma liczb c1 i c2 to: {suma}");
             Console.WriteLine($"Różnica liczb c1 i c2 to: {roznica}");
+            Console.WriteLine($"Iloczyn liczb c1 i c2 to: {iloczyn}");
         }
     }
 }
