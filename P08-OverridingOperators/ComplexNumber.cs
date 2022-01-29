1cd16a2b6e9266476be54909ecbc0690f541c0de
Jacek Nowicki <mopikx@gmail.com>
2022-01-29 12:01:36 +0100

    Complex Numbers

---
+++ P08-OverridingOperators/ComplexNumber.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08_OverridingOperators
+{
+    internal class ComplexNumber
+    {
+        public double Re { get; init; }
+        public double Im { get; init; }
+
+        public ComplexNumber(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Re + c2.Re, c1.Im + c2.Im);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Re - c2.Re, c1.Im - c2.Im);
+        }
+
+        public static bool operator ==(ComplexNumber c1, ComplexNumber c2)
+        {
+            return c1.Re == c2.Re && c1.Im == c2.Im;
+        }
+
+        public static bool operator !=(ComplexNumber c1, ComplexNumber c2)
+        {
+            return !(c1 == c2);
+        }
+
+        public override string ToString()
+        {
+            if (Im >= 0)
+                return $"{Re} + {Im}i";
+            return $"{Re} - {Math.Abs(Im)}i";
+        }
+    }
+}
---
+++ P08-OverridingOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P08_OverridingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber c1 = new ComplexNumber(5, 10);
+            ComplexNumber c2 = new ComplexNumber(20, 20);
+
+            ComplexNumber suma = c1 + c2;
+            ComplexNumber roznica = c1 - c2;
+
+            Console.WriteLine($"Suma liczb c1 i c2 to: {suma}");
+            Console.WriteLine($"Różnica liczb c1 i c2 to: {roznica}");
+        }
+    }
+}
